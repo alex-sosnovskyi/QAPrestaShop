c2c155a1ed99c1e851e8bf36a473545916655f43
Alex <s-sasha.pl@i.ua>
2017-09-30 00:03:01 +0300

    BaseDriver

--- src/main/java/ua/i/pl/sosnovskyi/BaseDriver.java
+++ src/main/java/ua/i/pl/sosnovskyi/BaseDriver.java
@@ -4,26 +4,59 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import ua.i.pl.sosnovskyi.utils.Properties;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by A Sosnovskyi on 29.09.2017.
  */
 public abstract class BaseDriver {
-    public WebDriver getDriver(String arg){
-        WebDriver result=null;
-        switch (arg){
+    private static WebDriver getDriver(){
+String browser= Properties.getBrowser();
+        switch (browser){
             case "chrome":{
-                result= new ChromeDriver();
-                break;
+                String key = System.getProperty("webdriver.chrome.driver");
+                String path=new File(BaseDriver.class.getResource("/chromedriver.exe").getFile()).getPath();
+                if (key == null) {
+                    System.setProperty("webdriver.chrome.driver", path);
+                }
+                return new ChromeDriver();
+
             }
-            case "firefox":{
-                result= new FirefoxDriver();
-                break;
+            case "gecko":{
+                String key = System.getProperty("webdriver.gecko.driver");
+                String path=new File(BaseDriver.class.getResource("/geckodriver.exe").getFile()).getPath();
+                if (key == null) {
+                    System.setProperty("webdriver.gecko.driver", path);
+                }
+                return new FirefoxDriver();
+
             }
             case "ie":{
-                result= new InternetExplorerDriver();
+                String key = System.getProperty("webdriver.ie.driver");
+                String path= new File(BaseDriver.class.getResource("/IEDriverServer.exe").getFile()).getPath();
+                if (key == null) {
+                    System.setProperty("webdriver.ie.driver", path);
+
+                }
+                return new InternetExplorerDriver();
+            }
+            default:{
+                throw  new RuntimeException("Wrong browser name");
             }
         }
-        return result;
+
+    }
+
+    public static WebDriver getConfiguredDriver(){
+        WebDriver driver=getDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        return driver;
+    }
+    public static void quitDriver(WebDriver driver){
+        driver.quit();
     }
 }
---
+++ src/main/java/ua/i/pl/sosnovskyi/utils/Properties.java
@@ -0,0 +1,52 @@
+package ua.i.pl.sosnovskyi.utils;
+
+import org.openqa.selenium.remote.BrowserType;
+
+/**
+ * Created by A Sosnovskyi on 22.09.2017.
+ */
+ public class Properties {
+    private static final String DEFAULT_BASE_URL = "http://prestashop-automation.qatestlab.com.ua/";
+    private static final String DEFAULT_BASE_ADMIN_URL = "http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/";
+    private static final String DEFAULT_BROWSER = BrowserType.CHROME;
+
+    /**
+     *
+     * @return Website frontend.
+     */
+    public static String getBaseUrl() {
+        return System.getProperty(EnvironmentVariables.BASE_URL.toString(), DEFAULT_BASE_URL);
+    }
+
+    /**
+     *
+     * @return Website backend (ULR of the Admin Panel.)
+     */
+    public static String getBaseAdminUrl() {
+        return System.getProperty(EnvironmentVariables.BASE_ADMIN_URL.toString(), DEFAULT_BASE_ADMIN_URL);
+    }
+
+    /**
+     *
+     * @return The name of the browser that need to be used for execution.
+     */
+    public static String getBrowser() {
+        return System.getProperty(EnvironmentVariables.BROWSER.toString(), DEFAULT_BROWSER);
+    }
+
+}
+enum EnvironmentVariables {
+    BASE_URL("env.url"),
+    BASE_ADMIN_URL("env.admin.url"),
+    BROWSER("browser");
+
+    private String value;
+    EnvironmentVariables(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
